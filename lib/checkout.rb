6425a076b171465b5826ceba311ebca6347320ba
AttyC <antoniajcronin@gmail.com>
2020-08-25 22:00:38 +0100

    developing tests for chekcout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,2 +1,10 @@
+require_relative 'item'
+
 class Checkout
+attr_reader :item
+attr_accessor :total
+
+  def scan(item)
+    @total = 1
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,5 +1,13 @@
 require 'checkout'
 
 describe Checkout do
+  it { is_expected.to respond_to(:scan).with(1).argument }
+
+  it 'scan method returns a subtotal' do
+    item = Item.new
+    subject.scan(item)
+    expect(subject.total).to eq 1
+
+  end
 
 end
